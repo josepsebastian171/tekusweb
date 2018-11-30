719c1b69c12f4207ac968527a776485b2ad5c709
José González <josepsebastian171@gmail.com>
2018-11-30 12:24:41 -0500

    View principal con lista clientes

--- tekusweb/Model/cliente.cs
+++ tekusweb/Model/cliente.cs
@@ -5,6 +5,7 @@ namespace Model
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("cliente")]
     public partial class cliente
@@ -27,5 +28,26 @@ namespace Model
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<servicio> servicio { get; set; }
+
+        public List<cliente> Listar()
+        {
+            var clientes = new List<cliente>();
+
+            try
+            {
+                using (var ctx = new tekusContext())
+                {
+                    clientes = ctx.cliente.ToList();
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return clientes;
+        }
+
     }
 }
--- tekusweb/tekusweb/Controllers/DefaultController.cs
+++ tekusweb/tekusweb/Controllers/DefaultController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
 
 namespace tekusweb.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        private cliente cliente = new cliente();
+        
         public ActionResult Index()
         {
-            return View();
+            return View(cliente.Listar());
         }
     }
 }
